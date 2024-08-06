7ae18b29aedff71dfb857c87dd663c88e16956d7
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2024-08-06 14:35:55 +0300

    Source File

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,143 @@
+// #include <Arduino.h>
+
+// // put function declarations here:
+// int myFunction(int, int);
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   int result = myFunction(2, 3);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
+
+
+// 
+// 
+// 
+// 
+
+
 #include <Arduino.h>
 #include "esp_camera.h"
+// #include "../.pio/libdeps/esp32dev/esp32-camera/driver/include/esp_camera.h"
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
-#include "FS.h"
-#include "SD_MMC.h"
-#include "SPI.h"
-#include "App_Camera.h"
 
-#include "../include/STD_TYPES.h"
-#include "../include/ERROR_STATE.h"
-#include "../include/BIT_MATH.h"
+// Replace with your network credentials
+const char* ssid = "Doaa Tawfik3";
+const char* password = "doaa22222";
 
-#include "../include/PIR_Interface.h"
+// Pin configuration for TTGO T-Camera
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM     4
+#define SIOD_GPIO_NUM    18
+#define SIOC_GPIO_NUM    23
+
+#define Y9_GPIO_NUM      36
+#define Y8_GPIO_NUM      37
+#define Y7_GPIO_NUM      38
+#define Y6_GPIO_NUM      39
+#define Y5_GPIO_NUM      35
+#define Y4_GPIO_NUM      14
+#define Y3_GPIO_NUM      13
+#define Y2_GPIO_NUM      34
+#define VSYNC_GPIO_NUM   5
+#define HREF_GPIO_NUM    27
+#define PCLK_GPIO_NUM    25
+
+AsyncWebServer server(80);
+
+void startCameraServer();
 
 void setup()
-{
-    Serial.begin(115200);
+ {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
 
-    /*When enabled, it allows the microcontroller to send extra debug information through the Serial port.
-     This can include internal states, errors and other diagnostic messages 
-     that are not part of the regular Serial output.*/
-    Serial.setDebugOutput(true);
-    Serial.println();
+ camera_config_t config;
+config.ledc_channel = LEDC_CHANNEL_0;
+config.ledc_timer = LEDC_TIMER_0;
+config.pin_d0 = Y2_GPIO_NUM;
+config.pin_d1 = Y3_GPIO_NUM;
+config.pin_d2 = Y4_GPIO_NUM;
+config.pin_d3 = Y5_GPIO_NUM;
+config.pin_d4 = Y6_GPIO_NUM;
+config.pin_d5 = Y7_GPIO_NUM;
+config.pin_d6 = Y8_GPIO_NUM;
+config.pin_d7 = Y9_GPIO_NUM;
+config.pin_xclk = XCLK_GPIO_NUM;
+config.pin_pclk = PCLK_GPIO_NUM;
+config.pin_vsync = VSYNC_GPIO_NUM;
+config.pin_href = HREF_GPIO_NUM;
+config.pin_sccb_sda = SIOD_GPIO_NUM;
+config.pin_sccb_scl = SIOC_GPIO_NUM;
+config.pin_pwdn = PWDN_GPIO_NUM;
+config.pin_reset = RESET_GPIO_NUM;
+config.xclk_freq_hz = 20000000;
+config.pixel_format = PIXFORMAT_JPEG;
+
+if (psramFound()) {
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+} else {
+  config.frame_size = FRAMESIZE_VGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
 }
 
-void loop()
-{
-  // put your main code here, to run repeatedly:
-  delay(10000);
+// Camera init
+esp_err_t err = esp_camera_init(&config);
+if (err != ESP_OK) {
+  Serial.printf("Camera init failed with error 0x%x", err);
+  return;
+}
+
+
+  // Wi-Fi connection
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  delay(1);
+}
+void startCameraServer() {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", "<html><body><h1>ESP32 Camera</h1><img src='/capture' /></body></html>");
+  });
+
+  server.on("/capture", HTTP_GET, [](AsyncWebServerRequest *request){
+    camera_fb_t * fb = NULL;
+    fb = esp_camera_fb_get();
+    if(!fb) {
+      Serial.println("Camera capture failed");
+      request->send(500, "text/plain", "Camera capture failed");
+      return;
+    }
+    request->send(200, "image/jpeg",(const uint8_t *)fb->buf, fb->len);
+    esp_camera_fb_return(fb);
+  });
+
+  server.begin();
 }
